ba65145013aaf82c06f7769686749bb4c0a6e82b
coffeephantom <coffeephantom@users.noreply.github.com>
2015-08-29 22:09:55 +0800

    修改文件名并按照教程写出数据库相关语句

---
+++ flaskr.py
@@ -0,0 +1,16 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__, instance_relative_config=True)
+# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connet_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
