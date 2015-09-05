ff347be4c3b9aca0205bf7e09cf71cb4a9f843ac
coffeephantom <coffeephantom@users.noreply.github.com>
2015-09-05 18:27:19 +0800

    修改顺序

--- flaskr.py
+++ flaskr.py
@@ -17,11 +17,18 @@ PASSWORD = 'admin'
 app.config.from_object(__name__)
 
 
-def connet_db():
+def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     # rv.row_factory = sqlite3.Row
     return rv
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 def get_db():
@@ -43,11 +50,7 @@ def close_db(error):
         g.db.close()
 
 
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+
 
 
 @app.route('/')
